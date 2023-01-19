4559fa7d84aa8483c27c72e1af41d2410f0f9080
Nathan Guimarães <nathangrs8@gmail.com>
2023-01-19 12:21:02 -0300

    Implementação de Formulário
    Definindo estilos e responsividade

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,42 @@
+
 <template>
-  <div class="app">
-      <Sidebar />
-      <router-view />
-     
-  </div>
+	<div class="app">
+		<!-- Sidebar -->
+		<Sidebar />
 
+		<!-- Content -->
+		<router-view />
+	</div>
 </template>
-
 
 <script setup>
 import Sidebar from './components/Sidebar.vue'
 </script>
 
 <style lang="scss">
-:root {
-    --primary: #4ade80;
-    --primary-alt: #22c55e;
-    --grey: #64748b;
-    --dark: #1e293b;
-    --dark-alt: #334155;
-    --light: #f1f5f9;
-    --sidebar-width: 300px;
-}
-* {
+
+/* Google Font Import - Poppins */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+*{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Fira sans', sans-serif;
+    font-family: 'Poppins', sans-serif;
 }
 
-body {
-    background: var(--light);
-}
+:root{
+    /* ===== Colors ===== */
+    --body-color: #EAE7E7;
+    --sidebar-color: #fffafa;
+    --primary-color: #19b42d;
+    --primary-color-light: #F6F5FF;
+    --toggle-color: #DDD;
+    --text-color: #707070;
 
-button {
-  cursor: pointer;
-  appearance: none;
-  border: none;
-  outline: none;
-  background: none;
-}
-
-.app {
-  display: flex;
-    
-  main {
-      flex: 1 1 0;
-      padding: 2rem;
-    
-      @media (max-width: 768px) {
-        padding-left: 6rem;
-      }
-  }
+    /* ====== Transition ====== */
+    --tran-03: all 0.2s ease;
+    --tran-03: all 0.3s ease;
+    --tran-04: all 0.3s ease;
+    --tran-05: all 0.3s ease;
 }
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,192 +1,105 @@
 <template>
-	<aside :class="`${is_expanded ? 'is-expanded' : ''}`">
-		<div class="logo">
-			<img :src="logoURL" alt="Vue" /> 
-			<span class="brand">SIGA</span>
-		</div>
+<body>
+	
+		<nav class="sidebar ">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="../assets/sigalogo.jpeg" alt="">
+                </span>
 
-		<div class="menu-toggle-wrap">
-			<button class="menu-toggle" @click="ToggleMenu">
-				<span class="material-icons">keyboard_double_arrow_right</span>
-			</button>
-		</div>
+                <div class="text logo-text">
+                    <span class="name">SIGA</span>
+                    <span class="profession"></span>
+                </div>
+            </div>
 
-		<h3>Menu</h3>
-		<div class="menu">
-			<router-link to="/" class="button">
-				<span class="material-icons">home</span>
-				<span class="text">Home</span>
-			</router-link>
-			<router-link to="/about" class="button">
-				<span class="material-icons">description</span>
-				<span class="text">About</span>
-			</router-link>
-			<router-link to="/team" class="button">
-				<span class="material-icons">group</span>
-				<span class="text">Team</span>
-			</router-link>
-			<router-link to="/contact" class="button">
-				<span class="material-icons">email</span>
-				<span class="text">Contact</span>
-			</router-link>
-		</div>
+            <i class='bx bx-chevron-right toggle'  onclick="openNav"></i>
+        </header>
 
-		<div class="flex"></div>
-		
-		<div class="menu">
-			<router-link to="/settings" class="button">
-				<span class="material-icons">settings</span>
-				<span class="text">Settings</span>
-			</router-link>
-		</div>
-	</aside>
+        <div class="menu-bar">
+            <div class="menu">
+
+                <li class="search-box">
+                    <i class='bx bx-search icon'></i>
+                    <input type="text" placeholder="Search...">
+                </li>
+
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <router-link to="/" class="button">
+                            <i class='bx bx-home-alt icon' ></i>
+                            <span class="text nav-text">Pagina Inicial</span>
+                        </router-link>
+                    </li>
+
+                    <li class="nav-link">
+                        <router-link to="/about" class="button">
+                            <i class='bx bx-bar-chart-alt-2 icon' ></i>
+                            <span class="text nav-text">Relatorio</span>
+						</router-link>
+                    </li>
+
+                    <li class="nav-link">
+                        <router-link to="" class="button">
+                            <i class='bx bx-bell icon'></i>
+                            <span class="text nav-text">Notificações</span>
+						</router-link>
+                    </li>
+
+                    <li class="nav-link">
+                        <router-link to="" class="button">
+                            <i class='bx bx-pie-chart-alt icon' ></i>
+                            <span class="text nav-text">Análises</span>
+						</router-link>
+                    </li>
+
+                    <li class="nav-link">
+                        <router-link to="" class="button">
+                            <i class='bx bx-cog icon' ></i>
+                            <span class="text nav-text">Configurações</span>
+						</router-link>
+                    </li>
+
+                    <li class="nav-link">
+                        <router-link to="" class="button">
+							<i class='bx bx-group icon'></i>
+                            <span class="text nav-text">Sobre Nós</span>
+						</router-link>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <router-link to="" class="button">
+                        <i class='bx bx-log-out icon' ></i>
+                        <span class="text nav-text">Logout</span>
+					</router-link>
+                </li>
+
+                <li class="mode">
+                    <div class="sun-moon">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Dark mode</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+                
+            </div>
+        </div>
+
+    </nav>
+
+	
+</body>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import logoURL from '../assets/sigalogo.jpeg'
-const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
-const ToggleMenu = () => {
-	is_expanded.value = !is_expanded.value
-	localStorage.setItem("is_expanded", is_expanded.value)
-}
-</script>
 
-<style lang="scss" scoped>
-aside {
-	display: flex;
-	flex-direction: column;
-	background-color: var(--dark);
-	color: var(--light);
-	width: calc(2rem + 32px);
-	overflow: hidden;
-	min-height: 100vh;
-	padding: 1rem;
-	transition: 0.2s ease-in-out;
-	.flex {
-		flex: 1 1 0%;
-	}
-	.brand{
-		font-weight: bold;
-		cursor:pointer;
-		display: flex;
-		margin-left: 50%;
-		margin-top:-20%;
-	}
-	.logo {
-		margin-bottom: 1rem;
-		img {
-			width: 5rem;
-		}
 
-	font-size: 24px;
- 	margin-right: 10px;
-  	cursor: pointer;
-  	padding-left: 10px;
-	
-}
-	.menu-toggle-wrap {
-		display: flex;
-		justify-content: flex-end;
-		margin-bottom: 1rem;
-		position: relative;
-		top: 0;
-		transition: 0.2s ease-in-out;
-		.menu-toggle {
-			transition: 0.2s ease-in-out;
-			.material-icons {
-				font-size: 2rem;
-				color: var(--light);
-				transition: 0.2s ease-out;
-			}
-			
-			&:hover {
-				.material-icons {
-					color: var(--primary);
-					transform: translateX(0.5rem);
-				}
-			}
-		}
-	}
-	h3, .button .text {
-		opacity: 0;
-		transition: opacity 0.3s ease-in-out;
-	}
-	h3 {
-		color: var(--grey);
-		font-size: 0.875rem;
-		margin-bottom: 0.5rem;
-		text-transform: uppercase;
-	}
-	.menu {
-		margin: 0 -1rem;
-		.button {
-			display: flex;
-			align-items: center;
-			text-decoration: none;
-			transition: 0.2s ease-in-out;
-			padding: 0.5rem 1rem;
-			.material-icons {
-				font-size: 2rem;
-				color: var(--light);
-				transition: 0.2s ease-in-out;
-			}
-			.text {
-				color: var(--light);
-				transition: 0.2s ease-in-out;
-			}
-			.brand{
-				color: var(--light);
-				transition: 0.2s ease-in-out;
 
-			}
-			&:hover {
-				background-color: var(--dark-alt);
-				.material-icons, .text {
-					color: var(--primary);
-				}
-			}
-			&.router-link-exact-active {
-				background-color: var(--dark-alt);
-				border-right: 5px solid var(--primary);
-				.material-icons, .text {
-					color: var(--primary);
-				}
-			}
-		}
-	}
-	.footer {
-		opacity: 0;
-		transition: opacity 0.3s ease-in-out;
-		p {
-			font-size: 0.875rem;
-			color: var(--grey);
-		}
-	}
-	&.is-expanded {
-		width: var(--sidebar-width);
-		.menu-toggle-wrap {
-			top: -3rem;
-			
-			.menu-toggle {
-				transform: rotate(-180deg);
-			}
-		}
-		h3, .button .text {
-			opacity: 1;
-		}
-		.button {
-			.material-icons {
-				margin-right: 1rem;
-			}
-		}
-		.footer {
-			opacity: 0;
-		}
-	}
-	@media (max-width: 1024px) {
-		position: absolute;
-		z-index: 99;
-	}
-}
-</style>
