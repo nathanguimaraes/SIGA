c9b83b37b1281be72f2db703ffea4deb601cb99d
Nathan Guimarães <33322455+nathanguimaraes@users.noreply.github.com>
2023-02-22 16:14:56 -0300

    Ajustes de responsividade

--- src/views/Relatorio.vue
+++ src/views/Relatorio.vue
@@ -3,9 +3,61 @@
         
     </head>
         <main class="home-page">
-            <h1>Sistema  de Inspeção Geográfica Agrícola</h1>
             
+           
+              <h1>Sistema  de Inspeção Geográfica Agrícola</h1>
+            <br>
             <section class="container">
+          <header>Informações do Produtor</header>
+          <form action="#" class="form">
+            <div class="input-box">
+              <label>Nome Completo</label>
+              <input type="text" placeholder="Insira o Nome Completo" required />
+            </div>
+    
+            <div class="input-box">
+              <label>CPF do Produtor</label>
+              <input type="text" placeholder="Insira o Nome Completo" required />
+            </div>
+    
+            <div class="input-box">
+              <label>Email</label>
+              <input type="text" placeholder="exemplo@email.com" required />
+            </div>
+    
+            <div class="column">
+              <div class="input-box">
+                <label>Telefone Celular</label>
+                <input type="text" placeholder="000 00000 0000" required />
+              </div>
+              <div class="input-box">
+                <label>Data de Nascimento</label>
+                <input type="date" placeholder="Enter birth date" required />
+              </div>
+            </div>
+            <div class="gender-box">
+              <h3>Genero</h3>
+              <div class="gender-option">
+                <div class="gender">
+                  <input type="radio" id="check-male" name="gender" checked />
+                  <label for="check-male">Masculino</label>
+                </div>
+                <div class="gender">
+                  <input type="radio" id="check-female" name="gender" />
+                  <label for="check-female">Feminino</label>
+                </div>
+                <div class="gender">
+                  <input type="radio" id="check-other" name="gender" />
+                  <label for="check-other">Outro</label>
+                </div>
+                <div class="gender">
+                  <input type="radio" id="check-not" name="gender" />
+                  <label for="check-other">Prefiro não declarar</label>
+                </div>
+              </div>
+            </div>    
+          </form>
+          <br>
           <header>Informações da Lavoura</header>
           <form action="#" class="form">
             <div class="column">
